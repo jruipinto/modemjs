0288e4dad9d21ba514990b921124d1b549674da7
J Rui Pinto <rui.jruipinto@gmail.com>
2019-12-26 12:15:24 +0000

    docs: disabled blog posts

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -41,7 +41,7 @@ const siteConfig = {
     {doc: 'doc1', label: 'Docs'},
     {doc: 'doc4', label: 'API'},
     {page: 'help', label: 'Help'},
-    {blog: true, label: 'Blog'},
+    {blog: false, label: null},
   ],
 
   // If you have users set above, you add it here:
