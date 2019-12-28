bea24b479d3e89c9a383098fca555fb0ca49a732
jruipinto <44852805+jruipinto@users.noreply.github.com>
2019-12-28 14:01:22 +0000

    docs: created script to move build files to docs
    created and implemented custom script (move-build-to-docs.js) to move
    website/build/modemjs  files to /docs folder automatically so that
    I can use  github's feature of hosting documentation website from
    repository's /docs folder

---
+++ website/move-build-to-docs.js
@@ -0,0 +1,28 @@
+const srcPath = './build/modemjs';
+const dstPath = '../docs';
+
+
+const fs = require('fs-extra');
+
+fs.remove(dstPath)
+    .then(() => {
+        console.log(`Cleaned ${dstPath} folder successfully.`)
+    })
+    .catch(err => {
+        console.error(`fs.remove(${dstPath}) error:`, err)
+    });
+
+fs.move(srcPath, dstPath, { overwrite: true })
+    .then(() => {
+        console.log(`Moved files in ${srcPath} to ${dstPath} folder successfully.`);
+        fs.remove('./build')
+            .then(() => {
+                console.log(`Removed build folder successfully.`)
+            })
+            .catch(err => {
+                console.error('fs.remove(\'./build\') error:', err)
+            });
+    })
+    .catch(err => {
+        console.error(`fs.move(${srcPath},${dstPath}) error:`, err)
+    });
