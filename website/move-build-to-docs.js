3900f43972cf81a7481e78f8977c536670b12b17
J Rui Pinto <rui.jruipinto@gmail.com>
2019-12-29 13:54:59 +0000

    docs(docusaurus): fixed bug on cleanig docs

--- website/move-build-to-docs.js
+++ website/move-build-to-docs.js
@@ -7,22 +7,23 @@ const fs = require('fs-extra');
 fs.remove(dstPath)
     .then(() => {
         console.log(`Cleaned ${dstPath} folder successfully.`)
+
+        fs.move(srcPath, dstPath, { overwrite: true })
+            .then(() => {
+                console.log(`Moved files in ${srcPath} to ${dstPath} folder successfully.`);
+                fs.remove('./build')
+                    .then(() => {
+                        console.log(`Removed build folder successfully.`)
+                    })
+                    .catch(err => {
+                        console.error('fs.remove(\'./build\') error:', err)
+                    });
+            })
+            .catch(err => {
+                console.error(`fs.move(${srcPath},${dstPath}) error:`, err)
+            });
+
     })
     .catch(err => {
         console.error(`fs.remove(${dstPath}) error:`, err)
-    });
-
-fs.move(srcPath, dstPath, { overwrite: true })
-    .then(() => {
-        console.log(`Moved files in ${srcPath} to ${dstPath} folder successfully.`);
-        fs.remove('./build')
-            .then(() => {
-                console.log(`Removed build folder successfully.`)
-            })
-            .catch(err => {
-                console.error('fs.remove(\'./build\') error:', err)
-            });
-    })
-    .catch(err => {
-        console.error(`fs.move(${srcPath},${dstPath}) error:`, err)
     });
